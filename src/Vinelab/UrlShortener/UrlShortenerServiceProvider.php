57789eaa53d4e21ab32f6aa42f43814c63d256bc
Mahmoud Zalt <mahmoud.z@msn.com>
2015-04-30 00:00:17 +0300

    structure setup

---
+++ src/Vinelab/UrlShortener/UrlShortenerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Vinelab\UrlShortener;
+
+use Illuminate\Support\ServiceProvider;
+
+class UrlShortenerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
