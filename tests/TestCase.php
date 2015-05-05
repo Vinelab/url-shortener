e53f4204b3700b5e1a85973702b3203f40ddc5c0
Mahmoud Zalt <mahmoud.z@msn.com>
2015-05-05 21:00:08 +0300

    add the TestCase and the phpunit config

---
+++ tests/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+namespace Vinelab\UrlShortener\Tests;
+
+use Mockery as M;
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+/**
+ * Class TestCase is the Parent test class that every test class must
+ * extend from.
+ *
+ * @category Parent TestCase
+ * @package Vinelab\UrlShortener\Tests
+ * @author  Mahmoud Zalt <mahmoud@vinelab.com>
+ */
+class TestCase extends PHPUnit
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+}
